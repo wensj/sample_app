702d07b90ff71755aeb733b18924b8ccea6c9072
wensj <wensujian815@gmail.com>
2013-10-03 23:20:06 +0800

    try to use bootstrap on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
-config.cache_classes = true
-config.serve_static_assets = true
-config.assets.compile = true
-config.assets.digest = true
