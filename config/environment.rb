fd69d3b08f51af669692b6d66cd7b645191da2ef
wensj <wensujian815@gmail.com>
2013-10-03 23:13:20 +0800

    heroku bug

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
-SampleApp::Application.configure do
-
-  # Force all access to the app over SSL, use Strict-Transport-Security,
-  # and use secure cookies.
-  config.force_ssl = true
-  config.cache_classes = true 
-  config.serve_static_assets = true 
-  config.assets.compile = true 
-  config.assets.digest = true
- 
-end
+config.cache_classes = true
+config.serve_static_assets = true
+config.assets.compile = true
+config.assets.digest = true
