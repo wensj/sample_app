5223d38904db5097408b958261bec64322dd5dd8
wensj <wensujian815@gmail.com>
2013-10-03 23:05:08 +0800

    try to use bootstrap on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,9 @@ SampleApp::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
-  config.assets.compile = true
+  config.cache_classes = true 
+  config.serve_static_assets = true 
+  config.assets.compile = true 
+  config.assets.digest = true
  
 end
--- config/environment.rb.bak
+++ config/environment.rb.bak
@@ -8,5 +8,6 @@ SampleApp::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+  config.assets.compile = true
  
 end
